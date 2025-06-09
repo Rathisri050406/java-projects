11ffd6f9c6bdc4bcdd95c622e5e0addfb9ad9aa2
rathi <rajarathisri@gmail.com>
2025-06-09 17:32:58 +0530

    basic operations

---
+++ main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello world");
+        System.out.println("hai");
+        int number=5;
+        System.out.println(number);
+        System.out.println(number+5);
+        System.out.println(number-3);
+        System.out.println(number*4);
+        System.out.println(number%2);
+        System.out.println(number/2);
+    }
+}
